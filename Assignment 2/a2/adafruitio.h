a7b70867ace6962a9a39c1890df9140c04cc92e8
Rahul Surti <rahulsurti97@gmail.com>
2024-02-08 18:31:44 -0800

    gitignoreg

--- Assignment 2/a2/adafruitio.h
+++ Assignment 2/a2/adafruitio.h
@@ -3,7 +3,7 @@
 #include "AdafruitIO_WiFi.h"
 
 #define IO_USERNAME "rahulsurti97"
-#define IO_KEY "aio_xhMi29FhzqGfvc6NxmxjiRdS5Xlx"
+#define IO_KEY "aio_GDCU892ARts6Zl0rBUIj8fTzce4O"
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASSWORD);
 
 AdafruitIO_Feed *_adafruitIoFeed= io.feed("stepcount");
