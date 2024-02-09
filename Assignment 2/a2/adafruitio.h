b4d0f44ff64f081ef5491f139a8dbbca3aeffd5b
Rahul Surti <rahulsurti97@gmail.com>
2024-02-08 18:33:00 -0800

    nit

--- Assignment 2/a2/adafruitio.h
+++
@@ -1,9 +0,0 @@
-#include "config.h"
-
-#include "AdafruitIO_WiFi.h"
-
-#define IO_USERNAME "rahulsurti97"
-#define IO_KEY "aio_GDCU892ARts6Zl0rBUIj8fTzce4O"
-AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASSWORD);
-
-AdafruitIO_Feed *_adafruitIoFeed= io.feed("stepcount");
